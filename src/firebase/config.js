c280a5685013b9adb5c53f9ea4e6cac955d6ee22
Traniel Pride <tranielmp8@gmail.com>
2021-06-10 09:05:52 -0500

    commit

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDTREj1toFC-KweadiTwgpgfQ3rwes5HVY",
+    authDomain: "vue-firebase-projects-a20b7.firebaseapp.com",
+    projectId: "vue-firebase-projects-a20b7",
+    storageBucket: "vue-firebase-projects-a20b7.appspot.com",
+    messagingSenderId: "390752390773",
+    appId: "1:390752390773:web:1af00583fed792881df23c"
+  };
+
+  //init firebase
+  firebase.initializeApp(firebaseConfig)
+
+  const projectFirestore = firebase.firestore()
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+  export { projectFirestore, timestamp }
